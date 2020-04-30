a16157dee4d7881f0a14af43001b7184750ab6ff
Nick-Jarvie <nickjarvie@gmail.com>
2020-05-01 10:44:09 +1200

    Add function three

--- classes.js
+++ classes.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
    one()
-   two() 
+   two()
+   three() 
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -24,5 +25,8 @@ function two () {
   one.classList.add('green')
 }
 // CREATE FUNCTION three HERE
-
+function three () {
+  var one = document.getElementById('three')
+  one.classList.add('red')
+}
 // CREATE FUNCTION makeVisible HERE
